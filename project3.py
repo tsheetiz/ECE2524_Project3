98332e67964ba1c63e68cda34995b8e01f72ba40
tsheetiz <tsheetiz@localhost.localdomain>
2020-05-01 19:14:55 -0400

    adding authentication for twitter

--- project3.py
+++ project3.py
@@ -1,2 +1,7 @@
 import tweepy
 import time, sys
+import keys
+
+#Authenticate
+auth = tweepy.OAuthHandler(keys.TWITTER_KEY, keys.TWITTER_SECRET_KEY)
+auth.set_access_token(keys.TWITTER_ACCESS_TOKEN, keys.TWITTER_ACCESS_TOKEN_SECRET)
