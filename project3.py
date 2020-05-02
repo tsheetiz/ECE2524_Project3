bf0242d8c78d7584a7fcc607226f1902aa80cf52
tsheetiz <tsheetiz@localhost.localdomain>
2020-05-02 18:53:44 -0400

    Created API Object, working on capturing tweets

--- project3.py
+++ project3.py
@@ -2,6 +2,9 @@ import tweepy
 import time, sys
 import keys
 
-#Authenticate
+# Authenticate
 auth = tweepy.OAuthHandler(keys.TWITTER_KEY, keys.TWITTER_SECRET_KEY)
 auth.set_access_token(keys.TWITTER_ACCESS_TOKEN, keys.TWITTER_ACCESS_TOKEN_SECRET)
+
+# Create API Object
+api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
