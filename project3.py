5facfd6fa403dd6cfbfe2089bef2e5218b5aa62d
tsheetiz <tsheetiz@localhost.localdomain>
2020-05-04 23:09:56 -0400

    added commits for libraries

--- project3.py
+++ project3.py
@@ -1,10 +1,18 @@
+# Tweepy library to access tweets
 from tweepy import OAuthHandler
 from tweepy import API
 from tweepy import Cursor
+
+# Libraries for time, system arguments
 import time, sys
 from datetime import datetime, date, time, timedelta
 from collections import Counter
+
+# Import keys from keys.py
 import keys
+
+# Profanity library that detects profanity
+from profanity import profanity
 
 # Authenticate
 auth = OAuthHandler(keys.TWITTER_KEY, keys.TWITTER_SECRET_KEY)
@@ -18,7 +26,7 @@ account_list = []
 if(len(sys.argv) > 1):
 	account_list = sys.argv[1:]
 else:
-	print("Please provide a list of usernames at the CMD.")
+	print("Add usernames to the end of the commandline")
 	sys.exit(0)
 
 if len(account_list) > 0:
