7f3b323f6d7bebda642d2e2877d1d276ae85cf39
tsheetiz <tsheetiz@localhost.localdomain>
2020-05-04 20:26:35 -0400

    if you place a username in the command line and run the file, you will get the following details for the account: name, screen_name, description, status count, friend and followers count

--- project3.py
+++ project3.py
@@ -1,28 +1,34 @@
-import tweepy
+from tweepy import OAuthHandler
+from tweepy import API
+from tweepy import Cursor
 import time, sys
+from datetime import datetime, date, time, timedelta
+from collections import Counter
 import keys
 
-class MyStreamListener(tweepy.StreamListener):
-	def __init__(self, api):
-		self.api = api
-		self.me = api.me()
-	
-	def on_status(self, tweet):
-		print(f"{tweet.user.name}:{tweet.text}")
-
-	def on_error(self, status):
-		print("Error Detected")
-
 # Authenticate
-auth = tweepy.OAuthHandler(keys.TWITTER_KEY, keys.TWITTER_SECRET_KEY)
+auth = OAuthHandler(keys.TWITTER_KEY, keys.TWITTER_SECRET_KEY)
 auth.set_access_token(keys.TWITTER_ACCESS_TOKEN, keys.TWITTER_ACCESS_TOKEN_SECRET)
 
 # Create API Object
-api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
+api = API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
 
-# Capture Tweet
-tweets_listener = MyStreamListener(api)
-stream = tweepy.Stream(api.auth, tweets_listener)
-stream.filter(track=["Python", "Django", "Tweepy"], languages=["en"])
+account_list = []
 
+if(len(sys.argv) > 1):
+	account_list = sys.argv[1:]
+else:
+	print("Please provide a list of usernames at the CMD.")
+	sys.exit(0)
+
+if len(account_list) > 0:
+	for target in account_list:
+		print("Getting data for " + target)
+		item = api.get_user(target)
+		print("name: " + item.name)
+		print("screen_name: " + item.screen_name)
+		print("description: " + item.description)
+		print("statuses_count: " + str(item.statuses_count))
+		print("friends_count: " + str(item.friends_count))
+		print("followers_count: " + str(item.followers_count))
 
