07d7f9d67078d2e35f89c30fcaae788d6cef7692
tsheetiz <tsheetiz@localhost.localdomain>
2020-05-06 19:13:13 -0400

    Added print statement that states average tweets per day

--- project3.py
+++ project3.py
@@ -47,4 +47,7 @@ delta = datetime.utcnow() - account_created_date
 account_age_days = delta.days
 print("Account age (in days): " + str(account_age_days))
 
+if account_age_days > 0:
+	print("Average tweets per day: " + "%.2f"%(float(tweets)/float(account_age_days)))
+
 
